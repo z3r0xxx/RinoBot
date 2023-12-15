10eda99dff9aafb715a071ea708742eb69c82187
z3r0x <40792831+z3r0xxx@users.noreply.github.com>
2023-12-15 16:00:39 +0300

    Добавил загрузку конфига из config.yaml

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 # created by zrx.
 
 import sys
+import yaml
 import time
 import asyncio
 import discord
@@ -78,7 +79,7 @@ async def unload(ctx, cog) -> None:
         await ctx.send(embed=discord.Embed(description=f'**ОШИБКА:** ```{e}```', color=0x8079fb))
     else:
         await ctx.send(embed=discord.Embed(description=f'Модуль `{cog}` успешно отключён от основной системы!',
-                                           color=0x8079fb))
+                                        color=0x8079fb))
 
 
 @bot.command()
@@ -127,12 +128,15 @@ async def on_ready():
 
 
 async def start():
-    # logger.debug('файл config.yml загружен.')
-    # logger.debug('найден конфиг config.yml, конфигурация загружена.')
-    # logger.debug('инициализация пакета source.')
     logger.debug(f'Python: {sys.version}')
     logger.debug(f'Discord.py: {discord.__version__}')
-    print()
+    
+    with open('settings.yml', 'r') as file:
+        data = yaml.safe_load(file)
+    bot.config = data
+    logger.debug('файл config.yml загружен.')
+    logger.debug('найден конфиг config.yml, конфигурация загружена.')
+    logger.debug('инициализация пакета source.')
 
     await bot.load_extension('jishaku')
     print()
@@ -140,10 +144,9 @@ async def start():
     total_characters_, total_code_characters_ = total_characters
     logger.info(f'Строк кода: {total_lines_}({total_code_lines}) Символов: {total_characters_}({total_code_characters_})')
 
-    await bot.start('NzIzMDg0NTQ4MTgyNzY5Njc1.Gcw12F.2bFgkufH2SL_vlD5wjy-sa1Nxw1ILl4KcSDABI')
+    await bot.start(bot.config.get("TOKEN"))
 
     
 
 if __name__ == "__main__":
-    
     asyncio.run(start())
